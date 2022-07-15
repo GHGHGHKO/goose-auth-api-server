2ce370048d4434e1e626c79037a4b9a0ad933cac
GHGHGHKO <gudrb963@gmail.com>
2022-07-15 23:14:04 +0900

    Initial commit

---
+++ src/main/java/me/synology/gooseauthapiserver/GooseAuthApiServerApplication.java
@@ -0,0 +1,13 @@
+package me.synology.gooseauthapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooseAuthApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooseAuthApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/synology/gooseauthapiserver/GooseAuthApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.synology.gooseauthapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooseAuthApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
