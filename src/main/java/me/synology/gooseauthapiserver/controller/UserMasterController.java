d149721420c3c5f2d1a6aec2c013d26661969d7f
GHGHGHKO <gudrb963@gmail.com>
2022-07-24 23:30:02 +0900

    feat: api x-auth-token 로그인 추가

--- src/main/java/me/synology/gooseauthapiserver/controller/UserMasterController.java
+++ src/main/java/me/synology/gooseauthapiserver/controller/UserMasterController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "1. UserMaster")
+@Tag(name = "2. UserMaster")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
