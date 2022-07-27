c4ad28a3cd01d5c3734201cd29d7343712cdb330
GHGHGHKO <gudrb963@gmail.com>
2022-07-27 23:10:19 +0900

    feat: api 회원가입 추가

--- src/main/java/me/synology/gooseauthapiserver/controller/SignController.java
+++ src/main/java/me/synology/gooseauthapiserver/controller/SignController.java
@@ -4,9 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import me.synology.gooseauthapiserver.model.response.CommonResult;
 import me.synology.gooseauthapiserver.model.response.SingleResult;
 import me.synology.gooseauthapiserver.service.ResponseService;
-import me.synology.gooseauthapiserver.service.SignInService;
+import me.synology.gooseauthapiserver.service.SignService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
-public class SignInController {
+public class SignController {
 
-  private final SignInService signInService;
+  private final SignService signService;
 
   private final ResponseService responseService;
 
   @Operation(summary = "로그인", description = "이메일 회원 로그인을 한다.")
-  @PostMapping(value = "signIn")
+  @PostMapping(value = "/signIn")
   public SingleResult<String> signIn(
       @Parameter(name = "회원ID : 이메일", required = true) @RequestParam String userEmail,
-      @Parameter(name = "비밀번호", required = true) @RequestParam String password) {
-    return responseService.getSingleResult(signInService.signIn(userEmail, password));
+      @Parameter(name = "비밀번호", required = true) @RequestParam String userPassword) {
+    return responseService.getSingleResult(signService.signIn(userEmail, userPassword));
+  }
+
+  @Operation(summary = "회원가입", description = "회원가입을 한다.")
+  @PostMapping(value = "/signUp")
+  public CommonResult signUp(
+      @Parameter(name = "회원ID : 이메일", required = true) @RequestParam String userEmail,
+      @Parameter(name = "비밀번호", required = true) @RequestParam String userPassword,
+      @Parameter(name = "이름", required = true) @RequestParam String userNickname) {
+    signService.signUp(userEmail, userPassword, userNickname);
+    return responseService.getSuccessResult();
   }
 }
