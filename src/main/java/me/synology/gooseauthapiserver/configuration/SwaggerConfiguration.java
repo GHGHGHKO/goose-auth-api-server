757c0ec84ff9b199a8c992036c3bd0f933d056b9
GHGHGHKO <gudrb963@gmail.com>
2022-07-30 23:54:30 +0900

    fix: springdoc set https

--- src/main/java/me/synology/gooseauthapiserver/configuration/SwaggerConfiguration.java
+++ src/main/java/me/synology/gooseauthapiserver/configuration/SwaggerConfiguration.java
@@ -1,9 +1,12 @@
 package me.synology.gooseauthapiserver.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,10 +28,13 @@ public class SwaggerConfiguration {
   @Bean
   public OpenAPI springGooseAuthAPI() {
     return new OpenAPI()
+        .addServersItem(new Server().url("https://goose-auth.synology.me"))
+        .components(new Components().addSecuritySchemes("basicScheme",
+            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
         .info(new Info().title("goose-auth-api Swagger")
             .description("goose-auth-api Swagger 문서입니다! 훈수는 환영합니다!")
             .version("v0.1.0")
-            .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .license(new License().name("Apache 2.0").url("https://springdoc.org")))
         .externalDocs(new ExternalDocumentation()
             .description("goose-auth-api GitHub")
             .url("https://github.com/GHGHGHKO/goose-auth-api-server"));
