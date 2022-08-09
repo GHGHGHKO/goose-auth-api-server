468c452580ade16f8be02c027249a2d114cbdc12
GHGHGHKO <gudrb963@gmail.com>
2022-08-09 22:40:32 +0900

    feat: add ResponseEntity and status code

--- src/main/java/me/synology/gooseauthapiserver/configuration/security/AuthenticationEntryPointCustom.java
+++ src/main/java/me/synology/gooseauthapiserver/configuration/security/AuthenticationEntryPointCustom.java
@@ -3,16 +3,23 @@ package me.synology.gooseauthapiserver.configuration.security;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
-@Component
+@Component("authenticationEntryPointCustom")
+@RequiredArgsConstructor
 public class AuthenticationEntryPointCustom implements AuthenticationEntryPoint {
+
+  @Qualifier("handlerExceptionResolver")
+  private final HandlerExceptionResolver resolver;
 
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException exception) throws IOException {
-    response.sendRedirect("/exception/entryPoint");
+    resolver.resolveException(request, response, null, exception);
   }
 }
