b62c22eab5acce491d7f64539663aab35dbbb32d
GHGHGHKO <gudrb963@gmail.com>
2022-08-31 21:30:07 +0900

    feat: GooseAuth 회원가입 추가하라

---
+++ src/main/java/me/synology/gooseauthapiserver/service/gooseauth/GooseAuthSignService.java
@@ -0,0 +1,46 @@
+package me.synology.gooseauthapiserver.service.gooseauth;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import me.synology.gooseauthapiserver.advice.UserExistExceptionCustom;
+import me.synology.gooseauthapiserver.dto.sign.GooseAuthSignUpRequestDto;
+import me.synology.gooseauthapiserver.entity.GooseAuthUserMaster;
+import me.synology.gooseauthapiserver.repository.GooseAuthUserMasterRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GooseAuthSignService {
+  private final PasswordEncoder passwordEncoder;
+
+  private final GooseAuthUserMasterRepository gooseAuthUserMasterRepository;
+
+  @Value("${info.api.id}")
+  private String apiUser;
+
+  public void signUp(GooseAuthSignUpRequestDto gooseAuthSignUpRequestDto) {
+    Optional<GooseAuthUserMaster> gooseAuthUserMaster = gooseAuthUserMasterRepository.findByUserEmail(
+        gooseAuthSignUpRequestDto.getUserEmail());
+
+    if (gooseAuthUserMaster.isPresent()) {
+      throw new UserExistExceptionCustom();
+    }
+
+    gooseAuthUserMasterRepository.save(
+        GooseAuthUserMaster.builder()
+            .userEmail(gooseAuthSignUpRequestDto.getUserEmail())
+            .userPassword(passwordEncoder.encode(gooseAuthSignUpRequestDto.getUserPassword()))
+            .userNickname(gooseAuthSignUpRequestDto.getUserNickname())
+            .userNickname(gooseAuthSignUpRequestDto.getUserNickname())
+            .passwordHint(gooseAuthSignUpRequestDto.getPasswordHint())
+            .createUser(apiUser)
+            .createDate(LocalDateTime.now())
+            .updateUser(apiUser)
+            .updateDate(LocalDateTime.now())
+            .build()
+    );
+  }
+}
