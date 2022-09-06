b049ce62532ddfb7e680d3fc28973d8b399ee15d
GHGHGHKO <gudrb963@gmail.com>
2022-09-06 21:40:21 +0900

    feat: dto에서 userEmail 제거하라

--- src/main/java/me/synology/gooseauthapiserver/dto/sign/gooseauth/AddItemRequestDto.java
+++ src/main/java/me/synology/gooseauthapiserver/dto/sign/gooseauth/AddItemRequestDto.java
@@ -1,20 +1,13 @@
 package me.synology.gooseauthapiserver.dto.sign.gooseauth;
 
 import java.util.List;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import me.synology.gooseauthapiserver.constants.RegularExpressionConstants;
 
 @Getter
 @Setter
 public class AddItemRequestDto {
-
-  @Email(regexp = RegularExpressionConstants.EMAIL_VALIDATION)
-  @NotBlank
-  private String userEmail;
 
   @NotNull
   private String name;
