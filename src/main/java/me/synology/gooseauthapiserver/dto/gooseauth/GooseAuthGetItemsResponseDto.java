386e3468ee45be54b5b7c07db3113022ef61b7e3
GHGHGHKO <gudrb963@gmail.com>
2022-09-22 22:56:56 +0900

    feat: get items dto 수정하라

--- src/main/java/me/synology/gooseauthapiserver/dto/gooseauth/GooseAuthGetItemsResponseDto.java
+++ src/main/java/me/synology/gooseauthapiserver/dto/gooseauth/GooseAuthGetItemsResponseDto.java
@@ -1,8 +1,5 @@
 package me.synology.gooseauthapiserver.dto.gooseauth;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,18 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GooseAuthGetItemsResponseDto {
 
+  private Long itemIdentity;
+
   private String name;
 
-  private String username;
-
-  private String password;
-
-  private List<String> uri;
+  private String userName;
 
   private String folder;
-
-  private String notes;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-  private LocalDateTime updateDate;
 }
---
+++ src/main/java/me/synology/gooseauthapiserver/repository/GooseAuthItemsUriRepository.java
@@ -0,0 +1,8 @@
+package me.synology.gooseauthapiserver.repository;
+
+import me.synology.gooseauthapiserver.entity.GooseAuthItemsUri;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GooseAuthItemsUriRepository extends JpaRepository<GooseAuthItemsUri, Long> {
+
+}
